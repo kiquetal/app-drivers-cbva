80d733fc7dd7e6f879229ff1b5dc4dcab4d258bf
Enrique M. Talavera <kiquetal@gmail.com>
2023-03-14 23:43:29 -0300

    fix tomorrow

--- screens/DetailQuestionary.js
+++ screens/DetailQuestionary.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import {View, Text, StyleSheet, ScrollView} from "react-native";
+import {View,  StyleSheet, ScrollView} from "react-native";
 import {useEffect, useState} from "react";
 import {supabase} from "../lib/supabase";
-import {Col, Row, Rows, Table, TableWrapper} from "react-native-table-component";
-import {Dialog} from "@rneui/themed";
+import {Cell, Col, Row, Rows, Table, TableWrapper} from "react-native-table-component";
+import {Dialog, Text} from "@rneui/themed";
 
 export default DetailQuestionaryScreen = (props) =>  {
 
@@ -41,7 +41,9 @@ export default DetailQuestionaryScreen = (props) =>  {
         ['Employment Information', 'What is your job title?'],
         ['Employment Information', 'How many years of experience do you have?'],
         ['Education Information', 'What is your highest level of education?'],
-        ['Education Information', 'What is your graduation year?'],
+
+        ['Education Information', 'What is your highest level of education?'],
+        ['COm', 'What is your graduation year?'],
     ];
 
     const sections = Array.from(new Set(tableData.slice(1).map(row => row[0])));
@@ -80,47 +82,14 @@ export default DetailQuestionaryScreen = (props) =>  {
     return (
         <ScrollView>
         <Dialog visible={isLoading}><Dialog.Loading></Dialog.Loading></Dialog>
-            <Table borderStyle={{borderWidth: 1}} style={{flexGrow:1}}>
-                <Row data={['SECTION1']} style={styles.head}/>
-                      <React.Fragment>
-                <Row data={['Posee usted los conocmientos minimos de PA, manejo y también curso de manejo defensivo, \n ocasionó una pérdida al cuartel']} style={styles.head}/>
-                          <TableWrapper style={styles.wrapper}>
-
-                    <Rows data={[["no yes"],[" nota larga"]]} flexArr={[2, 1, 1]} style={styles.row} textStyle={styles.text}/>
-                          </TableWrapper>
-                          </React.Fragment>
-
-                <Row data={['tiene usted experiencia en manejar?']}/>
-                <TableWrapper style={styles.wrapper} >
-                    <Rows data={[["no yes"],[" nota larga"]]} flexArr={[2, 1, 1]} style={styles.row} textStyle={styles.text}/>
-                </TableWrapper>
-
-                <Row data={['tiene usted experiencia en manejar?']}/>
-                <TableWrapper style={styles.wrapper} >
-                    <Rows data={[["no yes"],[" nota larga"]]} flexArr={[2, 1, 1]} style={styles.row} textStyle={styles.text}/>
-                </TableWrapper>
-
-                <Row data={['tiene usted experiencia en manejar?']}/>
-                <TableWrapper style={styles.wrapper} >
-                    <Rows data={[["no yes"],[" nota larga"]]} flexArr={[2, 1, 1]} style={styles.row} textStyle={styles.text}/>
-                </TableWrapper>
-                <Row data={['tiene usted experiencia en manejar?']}/>
-                <TableWrapper style={styles.wrapper} >
-                    <Rows data={[["no yes"],[" nota larga"]]} flexArr={[2, 1, 1]} style={styles.row} textStyle={styles.text}/>
-                </TableWrapper>
-                <Row data={['tiene usted experiencia en manejar?']}/>
-                <TableWrapper style={styles.wrapper} >
-                    <Rows data={[["no yes"],[" nota larga"]]} flexArr={[2, 1, 1]} style={styles.row} textStyle={styles.text}/>
-                </TableWrapper>
-                <Row data={['tiene usted experiencia en manejar?']}/>
-                <TableWrapper style={styles.wrapper} >
-                    <Rows data={[["no yes"],large?[" nota larga"]:null]} flexArr={[2, 1, 1]} style={styles.row} textStyle={styles.text}/>
-                </TableWrapper>
-                <Row data={['tiene usted experiencia en manejar?']}/>
-                <TableWrapper style={styles.wrapper} >
-                    <Rows data={[["no yes"],[" nota larga"]]} flexArr={[2, 1, 1]} style={styles.row} textStyle={styles.text}/>
-                </TableWrapper>
-
+            <Table borderStyle={{borderWidth: 1}} style={{borderColor: 'black'}}>
+                {sections.map((section, index) => {
+                 return (
+                    <Cell key={index} style={styles.row} textStyle={styles.text}>
+                        <Text style={styles.text}>asdadas</Text>
+                    </Cell>
+                 )
+                })}
 
             </Table>
         </ScrollView>
@@ -131,6 +100,6 @@ const styles = StyleSheet.create({
     head: {    backgroundColor: '#f1f8ff' , flex:1  },
     wrapper: { flexDirection: 'row' , borderStyle: 'solid', borderWidth: 1, borderColor: 'black'},
     title: { flex: 1, backgroundColor: '#f6f8fa' },
-    row: {  height: 48  },
-    text: { textAlign: 'center' }
+    row: {  height: 48 ,color:'red' },
+    text: { textAlign: 'center', color:'red' }
 });
