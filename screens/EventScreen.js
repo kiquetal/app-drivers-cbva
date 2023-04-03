dcaa1902c768662d4c1c84d499e7252f3761901a
Enrique M. Talavera <kiquetal@gmail.com>
2023-04-02 23:00:18 -0400

    Changing behaviour of maps

--- screens/EventScreen.js
+++ screens/EventScreen.js
@@ -5,6 +5,7 @@ import {useEffect, useRef, useState} from "react";
 import Icon from "react-native-vector-icons/FontAwesome";
 import * as Location from "expo-location";
 import {getForegroundPermissionsAsync} from "expo-location";
+import {Overlay} from "@rneui/themed";
 export default EventScreen = (props) =>  {
     const { navigation } = props;
 
@@ -37,55 +38,25 @@ export default EventScreen = (props) =>  {
     }
 
     const changeColorMarker = (item) => {
-        console.log("identificar marker", item);
-
         const updatedMarkers = markers.map((marker) => {
             if (marker.name === item.name) {
                 if (!marker.hasOwnProperty('color')) {
                 marker.color = 'red'; // replace with desired color
                 } else {
-                    console.log("have color", marker.color)
                     if (marker.color == 'red') {
                         marker.color = '#357eb9'; // replace with desired color
                     } else {
-                        console.log("change to red")
                         marker.color = 'red'; // replace with desired color
                     }
                 }
             }
-            console.log("marker", marker)
             return marker;
         });
         setMarkers(updatedMarkers);
 
-
-
-
     };
 
-    const [locationStatus, setLocationStatus] = useState();
 
-    const appState = useRef(AppState.currentState);
-    const [, setAppStateVisible] = useState(appState.current);
-
-    const handleAppStateChange = async (nextAppState) => {
-        if (appState.current.match(/inactive|background/) && nextAppState === 'active') {
-            console.log('App has come to the foreground!');
-            setLocationStatus(await getForegroundPermissionsAsync());
-        }
-
-        console.log("appState", appState.current);
-        appState.current = nextAppState;
-        setAppStateVisible(appState.current);
-    };
-
-    useEffect(() => {
-        console.log("gps change")
-        const subscription = AppState.addEventListener('change', handleAppStateChange);
-     //   return () => {
-     //       subscription.remove();
-     //   };
-    }, []);
 
     useEffect(() => {
         const getLocation= async () => {
@@ -94,7 +65,6 @@ export default EventScreen = (props) =>  {
                 console.log('Permission to access location was denied');
                 return;
             }
-            console.log(await  Location.hasServicesEnabledAsync())
             try {
             //    let statusTwo = await Location.requestForegroundPermissionsAsync();
              //   console.log("location", statusTwo);
@@ -106,7 +76,7 @@ export default EventScreen = (props) =>  {
                 console.log("exception- obtaining location", e);
             }
         }
-        getLocation().then(r => console.log("r", r)).catch(e => console.log("exception whey", e));
+          getLocation();
     },[navigation]);
 
 
@@ -117,7 +87,6 @@ export default EventScreen = (props) =>  {
 
                 <Mapbox.MapView style={styles.map}
                                 onPress={(event) => {
-                                    console.log(JSON.stringify(event.geometry))
                                     addMarker(
                                         event.geometry.coordinates[0],
                                         event.geometry.coordinates[1],
@@ -134,10 +103,9 @@ export default EventScreen = (props) =>  {
                         <Mapbox.MarkerView
                             key={`marker-${index}`}
                             coordinate={[ marker.longitude, marker.latitude]}
-
                             title={marker.name}
                         >
-                            <Icon name={'map-marker'}  iconColor='red' size={30} color={ marker.color ? marker.color: '#357eb9'}  onPress={()=>removeMarker(index)}  />
+                            <Icon name={'map-marker'}  style={{shadowColor:'black'}}  iconColor='red' size={30} color={ marker.color ? marker.color: '#357eb9'}  onPress={()=>removeMarker(index)}  />
                         </Mapbox.MarkerView>
                     ))}
                 </Mapbox.MapView>
@@ -155,7 +123,6 @@ export default EventScreen = (props) =>  {
                     <TextInput
                         style={{fontSize: 20, marginVertical: 20, borderWidth: 1, padding: 10, width: 70}}
                         value={newMarkerName}
-
                         onChangeText={setNewMarkerName}
                     />
                     <Button title="Guardar" onPress={saveMarker} />
