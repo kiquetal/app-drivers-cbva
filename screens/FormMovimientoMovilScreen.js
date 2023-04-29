9844d8344495b6901e8c300818f68b606ca1631a
Enrique M. Talavera <kiquetal@gmail.com>
2023-04-29 01:26:52 -0400

    adding form

--- screens/FormMovimientoMovilScreen.js
+++ screens/FormMovimientoMovilScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {View,Text} from "react-native";
+import {View, Text, ScrollView} from "react-native";
 import { useForm, Controller} from "react-hook-form";
 import { Dropdown} from "react-native-element-dropdown";
-import { Text as TextRNE } from "@rneui/themed";
+import {Card, Text as TextRNE, Input, Button} from "@rneui/themed";
+
 export default FormMovimientoMovilScreen = (props) => {
 
-
+    const [tipoDeServicio, setTipoDeServicio] = React.useState("");
+    const [movil, setMovil] = React.useState("");
+    const [conductor, setConductor] = React.useState("");
     const { navigation } = props;
     const { control, handleSubmit, formState: { errors } } = useForm();
 
@@ -15,15 +18,86 @@ export default FormMovimientoMovilScreen = (props) => {
         {label: "F2",
         value: "F2"},
     ]
+    const conductores_data=[{
+        label:"M1-TTE GILL",value:"M1-TTE GILL"
+    },{
+        label:"M2-JOSE MONTORFANO", value: "M2-JOSE MONTORFANO"
+    },{
+        label:"--", value: "--"
+    }]
+
+    const sercivicio_tipos = [{
+        label:"10.40", value: "10.40",
+    }, {
+        label: "10.41", value: "10.41",
+    },{
+        label: "10.42", value: "10.42",
+    }];
 
     return (
-        <View>
+        <ScrollView>
+            <Card containerStyle={{backgroundColor:"white"}}>
+                <Card.Title style={{color:"black"}}>Movimiento de móviles</Card.Title>
         <TextRNE style={{color:"black"}}  >Movil</TextRNE>
+                <Controller  name="movil"
+                             control={control}
+                             render={({field: {onChange, onBlur,value},fieldState}) => (
         <Dropdown data={movil_data}
                   placeholder={"Seleccione un movil"}
-                  labelField="label" valueField="value" onChange={item => console.log(item)} />
+                  labelField="label" valueField="value"
+                  onChange={item => {
+                       onChange(item.value)
+                  }
+                  }
+                  key="movil"
+                  value={value}
+        />
+    )} defaultValue={""} />
 
-        </View>
+         <TextRNE style={{color:"black"}}  >KM DE LLEGADA</TextRNE>
+         <Input placeholder="Ingrese Km" inputStyle={{color:"black"}}   ></Input>
+            </Card>
+
+            <Card containerStyle={{backgroundColor:"white"}}>
+                <Card.Title style={{color:"black"}}>Conductor</Card.Title>
+                <TextRNE style={{color:"black"}}  >10.30</TextRNE>
+                <Dropdown data={conductores_data}
+                          placeholder={"Seleccione 10.30"}
+                          labelField="label" valueField="value" onChange={item => setConductor(item.value)}
+                          value={conductor}
+                key="conductor"
+                />
+
+                <TextRNE style={{color:"black"}}  >10.30 OTRO (BA)</TextRNE>
+                <Input placeholder="Ingrese BA" inputStyle={{color:"black"}}   ></Input>
+            </Card>
+            <Card containerStyle={{backgroundColor:"white"}}>
+                <Card.Title style={{color:"black"}}>Tipo de Servicio</Card.Title>
+                <TextRNE style={{color:"black"}}  >Seleccione el servicio</TextRNE>
+                <Dropdown data={sercivicio_tipos}
+                          placeholder={"seleccione"}
+                          labelField="label" valueField="value"
+                          onChange={item => {
+                              console.log(item)
+                              setTipoDeServicio(item.value)
+                              return item.value
+                            }
+
+                          }
+                          value={tipoDeServicio}
+                key="servicio"/>
+
+                { tipoDeServicio === "10.40" &&
+                <View>
+                    <TextRNE style={{color:"black"}}  >10.40 OTRO (BA)</TextRNE>
+                    <Input placeholder="Ingrese BA" inputStyle={{color:"black"}}   ></Input>
+                </View>
+                }
+
+
+            </Card>
+            <Button buttonStyle={{marginTop:10}} onPress={handleSubmit(data => console.log(data))}>Guardar</Button>
+        </ScrollView>
     )
 
 }
