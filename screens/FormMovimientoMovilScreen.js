b6988caeedf1f298ad166795b32e2faaeee23547
Enrique M. Talavera <kiquetal@gmail.com>
2023-04-29 00:11:01 -0400

    adding library

--- screens/FormMovimientoMovilScreen.js
+++ screens/FormMovimientoMovilScreen.js
@@ -2,16 +2,26 @@ import React from 'react';
 import {View,Text} from "react-native";
 import { useForm, Controller} from "react-hook-form";
 import { Dropdown} from "react-native-element-dropdown";
-
+import { Text as TextRNE } from "@rneui/themed";
 export default FormMovimientoMovilScreen = (props) => {
 
 
     const { navigation } = props;
     const { control, handleSubmit, formState: { errors } } = useForm();
 
+    const movil_data = [
+        { label: "F1",
+        value: "F1"},
+        {label: "F2",
+        value: "F2"},
+    ]
 
     return (
         <View>
+        <TextRNE style={{color:"black"}}  >Movil</TextRNE>
+        <Dropdown data={movil_data}
+                  placeholder={"Seleccione un movil"}
+                  labelField="label" valueField="value" onChange={item => console.log(item)} />
 
         </View>
     )
